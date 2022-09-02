57b2e5af020a7311b9f65f21747b1fb984f90713
Eli Kinsey <eli@ekinsey.dev>
2022-09-02 16:27:59 -0700

    initial commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,46 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
-// You can delete this file if you're not using it
+const axios = require("axios");
 
-/**
- * You can uncomment the following line to verify that
- * your plugin is being loaded in your site.
- *
- * See: https://www.gatsbyjs.com/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
- */
-exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
+exports.sourceNodes = async (
+  { createContentDigest, actions: { createNode } },
+  options
+) => {
+  const { apiKey } = options;
+  if (!apiKey)
+    return console.error("gatsby-source-ashby: Missing options.apiKey");
+
+  async function getJobs() {
+    const requestOptions = {
+      method: "POST",
+      url: "https://api.ashbyhq.com/jobPosting.list",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Basic ${Buffer.from(`${apiKey}:`).toString("base64")}`,
+      },
+    };
+
+    try {
+      const {
+        data: { results },
+      } = await axios.request(requestOptions);
+      return results;
+    } catch (err) {
+      console.error(`gatsby-source-ashby: ${err}`);
+    }
+  }
+
+  const jobs = await getJobs();
+  if (!jobs) return;
+  jobs.forEach((job) => {
+    const node = {
+      ...job,
+      parent: null,
+      children: [],
+      internal: {
+        type: `AshbyJob`,
+        contentDigest: createContentDigest(job),
+      },
+    };
+    createNode(node);
+  });
+};
